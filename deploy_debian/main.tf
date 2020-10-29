0ab50c7de78da72f43288cf2c388407d46de6583
giovannisavino <gsavino@gsavinoX.local>
2020-10-29 03:32:33 +0100

    Added var

--- deploy_debian/main.tf
+++ deploy_debian/main.tf
@@ -48,5 +48,5 @@ resource "ibm_compute_vm_instance" "debian_small_virtual_guest" {
 }
 
 output "vm_ip" {
-  value = "Public : ${ibm_compute_vm_instance.debian_small_virtual_guest.ipv4_address}"
+  value = "${ibm_compute_vm_instance.debian_small_virtual_guest.ipv4_address}"
 }
