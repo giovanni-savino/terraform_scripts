dc56e462b4e99032b25c61ffcf2346bc80952b81
giovannisavino <gsavino@gsavinoX.local>
2020-11-18 22:23:09 +0100

    Changing terraform template

--- deploy_postgres_azure/main.tf
+++ deploy_postgres_azure/main.tf
@@ -1,84 +1,219 @@
 ############### Input Var #############
 
-
-variable "postgresql-admin-login" {
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the PostgreSQL Server. Changing this forces a new resource to be created."
   type        = string
-  description = "Login to authenticate to PostgreSQL Server"
-}
-variable "postgresql-admin-password" {
-  type        = string
-  description = "Password to authenticate to PostgreSQL Server"
+  default     = "kopi-postgresql-rg"
 }
 
-variable "postgresql-version" {
+variable "location" {
+  description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
   type        = string
-  description = "PostgreSQL Server version to deploy"
-  default     = "9"
+  default     = "North Europe"
 }
-variable "postgresql-sku-name" {
+
+variable "server_name" {
+  description = "Specifies the name of the PostgreSQL Server. Changing this forces a new resource to be created."
   type        = string
-  description = "PostgreSQL SKU Name"
-  default     = "B_Gen5_1"
+  default     = "AzurePostgres"
 }
-variable "postgresql-storage" {
+
+variable "sku_name" {
+  description = "Specifies the SKU Name for this PostgreSQL Server. The name of the SKU, follows the tier + family + cores pattern (e.g. B_Gen4_1, GP_Gen5_8)."
   type        = string
-  description = "PostgreSQL Storage in MB"
-  default     = "5120"
+  default     = "GP_Gen5_1"
+}
+
+variable "storage_mb" {
+  description = "Max storage allowed for a server. Possible values are between 5120 MB(5GB) and 1048576 MB(1TB) for the Basic SKU and between 5120 MB(5GB) and 4194304 MB(4TB) for General Purpose/Memory Optimized SKUs."
+  type        = number
+  default     = 5120
+}
+
+variable "backup_retention_days" {
+  description = "Backup retention days for the server, supported values are between 7 and 35 days."
+  type        = number
+  default     = 7
+}
+
+variable "geo_redundant_backup_enabled" {
+  description = "Enable Geo-redundant or not for server backup. Valid values for this property are Enabled or Disabled, not supported for the basic tier."
+  type        = bool
+  default     = false
+}
+
+variable "administrator_login" {
+  description = "The Administrator Login for the PostgreSQL Server. Changing this forces a new resource to be created."
+  type        = string
+}
+
+variable "administrator_password" {
+  description = "The Password associated with the administrator_login for the PostgreSQL Server."
+  type        = string
+}
+
+variable "server_version" {
+  description = "Specifies the version of PostgreSQL to use. Valid values are 9.5, 9.6, and 10.0. Changing this forces a new resource to be created."
+  type        = string
+  default     = "9.5"
+}
+
+variable "ssl_enforcement_enabled" {
+  description = "Specifies if SSL should be enforced on connections. Possible values are Enabled and Disabled."
+  type        = bool
+  default     = true
+}
+
+variable "db_names" {
+  description = "The list of names of the PostgreSQL Database, which needs to be a valid PostgreSQL identifier. Changing this forces a new resource to be created."
+  type        = list(string)
+  default     = []
+}
+
+variable "db_charset" {
+  description = "Specifies the Charset for the PostgreSQL Database, which needs to be a valid PostgreSQL Charset. Changing this forces a new resource to be created."
+  type        = string
+  default     = "UTF8"
+}
+
+variable "db_collation" {
+  description = "Specifies the Collation for the PostgreSQL Database, which needs to be a valid PostgreSQL Collation. Note that Microsoft uses different notation - en-US instead of en_US. Changing this forces a new resource to be created."
+  type        = string
+  default     = "English_United States.1252"
+}
+
+variable "firewall_rule_prefix" {
+  description = "Specifies prefix for firewall rule names."
+  type        = string
+  default     = "firewall-"
+}
+
+variable "firewall_rules" {
+  description = "The list of maps, describing firewall rules. Valid map items: name, start_ip, end_ip."
+  type        = list(map(string))
+  default     = []
+}
+
+variable "vnet_rule_name_prefix" {
+  description = "Specifies prefix for vnet rule names."
+  type        = string
+  default     = "postgresql-vnet-rule-"
+}
+
+variable "vnet_rules" {
+  description = "The list of maps, describing vnet rules. Valud map items: name, subnet_id."
+  type        = list(map(string))
+  default     = []
+}
+
+variable "tags" {
+  description = "A map of tags to set on every taggable resources. Empty by default."
+  type        = map(string)
+  default     = {}
+}
+
+variable "postgresql_configurations" {
+  description = "A map with PostgreSQL configurations to enable."
+  type        = map(string)
+  default     = {}
 }
 
 ############### Main #############
 
 
-resource "azurerm_resource_group" "postgresql-rg" {
-  name     = "kopi-postgresql-rg"
-  location = "North Europe"
+resource "azurerm_postgresql_server" "server" {
+  name                = var.server_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  sku_name = var.sku_name
+
+  storage_mb                   = var.storage_mb
+  backup_retention_days        = var.backup_retention_days
+  geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
+
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_password
+  version                      = var.server_version
+  ssl_enforcement_enabled      = var.ssl_enforcement_enabled
+
+  tags = var.tags
+}
+
+resource "azurerm_postgresql_database" "dbs" {
+  count               = length(var.db_names)
+  name                = var.db_names[count.index]
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.server.name
+  charset             = var.db_charset
+  collation           = var.db_collation
+}
+
+resource "azurerm_postgresql_firewall_rule" "firewall_rules" {
+  count               = length(var.firewall_rules)
+  name                = format("%s%s", var.firewall_rule_prefix, lookup(var.firewall_rules[count.index], "name", count.index))
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.server.name
+  start_ip_address    = var.firewall_rules[count.index]["start_ip"]
+  end_ip_address      = var.firewall_rules[count.index]["end_ip"]
+}
+
+resource "azurerm_postgresql_virtual_network_rule" "vnet_rules" {
+  count               = length(var.vnet_rules)
+  name                = format("%s%s", var.vnet_rule_name_prefix, lookup(var.vnet_rules[count.index], "name", count.index))
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.server.name
+  subnet_id           = var.vnet_rules[count.index]["subnet_id"]
+}
+
+resource "azurerm_postgresql_configuration" "db_configs" {
+  count               = length(keys(var.postgresql_configurations))
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.server.name
+
+  name  = element(keys(var.postgresql_configurations), count.index)
+  value = element(values(var.postgresql_configurations), count.index)
 }
 
 
-resource "azurerm_postgresql_server" "postgresql-server" {
-  name = "kopi-postgresql-server"
-  location = azurerm_resource_group.postgresql-rg.location
-  resource_group_name = azurerm_resource_group.postgresql-rg.name
- 
-  administrator_login          = var.postgresql-admin-login
-  administrator_login_password = var.postgresql-admin-password
- 
-  sku_name = var.postgresql-sku-name
-  version  = var.postgresql-version
- 
-  storage_mb        = var.postgresql-storage
-  auto_grow_enabled = true
-  
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false  
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+############### Output #############
+
+output "server_name" {
+  description = "The name of the PostgreSQL server"
+  value       = azurerm_postgresql_server.server.name
 }
 
-
-resource "azurerm_postgresql_database" "postgresql-db" {
-  name                = "kopidb"
-  resource_group_name = azurerm_resource_group.postgresql-rg.name
-  server_name         = azurerm_postgresql_server.postgresql-server.name
-  charset             = "utf8"
-  collation           = "English_United States.1252"
+output "server_fqdn" {
+  description = "The fully qualified domain name (FQDN) of the PostgreSQL server"
+  value       = azurerm_postgresql_server.server.fqdn
 }
 
-
-resource "azurerm_postgresql_firewall_rule" "postgresql-fw-rule" {
-  name                = "PostgreSQL Office Access"
-  resource_group_name = azurerm_resource_group.postgresql-rg.name
-  server_name         = azurerm_mysql_server.postgresql-server.name
-  start_ip_address    = "210.170.94.100"
-  end_ip_address      = "210.170.94.120"
+output "administrator_login" {
+  value = var.administrator_login
 }
 
+output "administrator_password" {
+  value     = var.administrator_password
+  sensitive = true
+}
 
-output "postgresql_server" {
-  value = azurerm_postgresql_server.postgresql-server
+output "server_id" {
+  description = "The resource id of the PostgreSQL server"
+  value       = azurerm_postgresql_server.server.id
 }
-provider "azurerm" {
-  version = "=2.0.0"
-  features {}
+
+output "database_ids" {
+  description = "The list of all database resource ids"
+  value       = [azurerm_postgresql_database.dbs.*.id]
 }
+
+output "firewall_rule_ids" {
+  description = "The list of all firewall rule resource ids"
+  value       = [azurerm_postgresql_firewall_rule.firewall_rules.*.id]
+}
+
+output "vnet_rule_ids" {
+  description = "The list of all vnet rule resource ids"
+  value       = [azurerm_postgresql_virtual_network_rule.vnet_rules.*.id]
+}
+
